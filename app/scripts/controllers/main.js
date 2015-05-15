754af74d7e1cf156166f5641fe0ddb16a7fd3e4e
Edwin Lunandy <populationtwo@gmail.com>
2015-05-14 22:10:46 -0400

    Generate app with Yeoman.

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name kopiAromaApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the kopiAromaApp
+ */
+angular.module('kopiAromaApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
