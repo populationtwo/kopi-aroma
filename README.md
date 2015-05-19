2ae197bbab8f96800eda5bbc40dbf33a25b3b89b
Edwin Lunandy <populationtwo@gmail.com>
2015-05-18 21:19:48 -0400

    Clean up and refactor code.

--- README.md
+++ README.md
@@ -5,8 +5,5 @@ version 0.11.1.
 
 ## Build & development
 
+Run `npm install` and `bower install`.
 Run `grunt` for building and `grunt serve` for preview.
-
-## Testing
-
-Running `grunt test` will run the unit tests with karma.
--- app/scripts/controllers/feed.js
+++ app/scripts/controllers/feed.js
@@ -8,15 +8,6 @@
  * Controller of the kopiAromaApp
  */
 angular.module( 'kopiAromaApp' )
-	//.controller('FeedCtrl', function ($scope) {
-	//	$scope.awesomeThings = [
-	//		'HTML5 Boilerplate',
-	//		'AngularJS',
-	//		'Karma'
-	//	];
-	//});
-
-
 	.controller( 'FeedCtrl', function ($scope, purchaseDataFactory) {
 		purchaseDataFactory.fetch().then( function (data) {
 			$scope.feeds = data;
